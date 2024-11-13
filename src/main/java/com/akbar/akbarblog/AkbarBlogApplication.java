3ab65b79ddd1d3556c0ecf6a78925b7d1e08a42a
akbar <akbar4857@qq.com>
2024-11-13 13:12:16 +0800

    初始化仓库！

---
+++ src/main/java/com/akbar/akbarblog/AkbarBlogApplication.java
@@ -0,0 +1,13 @@
+package com.akbar.akbarblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AkbarBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AkbarBlogApplication.class, args);
+    }
+
+}
