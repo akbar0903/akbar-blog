3e27d6eb0029ba5e64c39e3b4b5bf508f3cb7bcb
akbar <akbar4857@qq.com>
2024-11-26 23:28:22 +0800

    feat:实现了用户头像列表删除功能

--- src/main/java/com/akbar/controller/UserAvatarHistoryController.java
+++ src/main/java/com/akbar/controller/UserAvatarHistoryController.java
@@ -6,10 +6,7 @@ import com.akbar.utils.Result;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/userAvatarHistory")
@@ -38,5 +35,20 @@ public class UserAvatarHistoryController {
         Page<UserAvatarHistory> pageResult = userAvatarHistoryService.page(page, queryWrapper);
 
         return Result.success(pageResult);
+    }
+
+    /**
+     * 删除用户头像历史记录
+     * @param id
+     * @return
+     */
+    @DeleteMapping("/{id}")
+    public Result deleteUserAvatarHistory(@PathVariable Integer id) {
+        boolean result = userAvatarHistoryService.removeById(id);
+
+        if (!result) {
+            return Result.success("删除失败！");
+        }
+        return Result.success("删除成功！");
     }
 }
